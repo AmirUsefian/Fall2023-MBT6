6bbca0e55226214abc0c493bba2611e7f169d2c3
amir <amiryn996@gmail.com>
2023-11-20 19:00:07 +0330

    Random library added

--- test2.cpp
+++ test2.cpp
@@ -3,41 +3,92 @@
 #include <fstream>
 #include <chrono>
 #include <cmath>
+#include <random>
+#include <string>
+
 using namespace std;
 
-class FileWriter {
+struct Config
+{
+    int randomNumbers = 30;
+    int minRandom = 20;
+    int maxRandom = 11111;
+};
+
+class FileWriter
+{
 public:
-    void writeNumbersToFile(const vector<int>& numbers, const string& filename) {
+    vector<int> generateRandomNumbers(vector<int> &numbers, int nums, int min = 0, int max = 1000)
+    {
+        // Create a random number generator engine
+        random_device rd;
+        mt19937 gen(rd()); // Mersenne Twister engine
+
+        // Create a distribution for the random numbers
+        uniform_int_distribution<int> distribution(min, max);
+
+        // Generate and append 'nums' random numbers to the vector
+        for (int i = 0; i < nums; ++i)
+        {
+            int random_number = distribution(gen);
+            numbers.push_back(random_number);
+        }
+        return numbers;
+    }
+
+    void writeNumbersToFile(const vector<int> &numbers, const string &filename)
+    {
+
         ofstream outFile(filename);
-        if (outFile.is_open()) {
-            for (int num : numbers) {
-                outFile << num << " ";
+        if (outFile.is_open())
+        {
+            for (int num : numbers)
+            {
+                outFile << num << endl;
             }
             outFile.close();
             cout << "Numbers have been written to " << filename << endl;
-        } else {
-            cout << "Unable to open file for writing" << endl;
+        }
+        else
+        {
+            cerr << "Unable to open file for writing" << endl;
         }
     }
 
-    void measureTimeTaken() {
-        auto start = chrono::high_resolution_clock::now();
+    void measureTimeTaken()
+    {
         // Simulate some work being done
-        for (int i = 0; i < 1000000; ++i) {
+        for (int i = 0; i < 1000000; ++i)
+        {
             int x = i * i;
             double y = sqrt(x); // Using math library function
         }
-        auto end = chrono::high_resolution_clock::now();
-        chrono::duration<double> timeTaken = end - start;
-        cout << "Time taken to write numbers to a file and do some work: " << timeTaken.count() << " seconds" << endl;
+    }
+
+    std::chrono::high_resolution_clock::time_point Record(){
+        return std::chrono::high_resolution_clock::now();
+    }
+
+    void timeTaken(auto start, auto end, string description){
+        chrono::duration<double> duration = end - start;
+        cout << "Time taken to " << description << ": " << duration.count() << " seconds" << endl;
     }
 };
 
-int main() {
-    vector<int> numbers = {1, 2, 3, 4, 5};
+int main()
+{
+    Config cfg;
+
+    std::vector<int> numbers;
     FileWriter fileWriter;
+
+    auto start = fileWriter.Record();
+    numbers = fileWriter.generateRandomNumbers(numbers, cfg.randomNumbers, cfg.minRandom, cfg.maxRandom);
+
     fileWriter.writeNumbersToFile(numbers, "numbers.txt");
     fileWriter.measureTimeTaken();
 
+    auto end = fileWriter.Record();
+    fileWriter.timeTaken(start, end, "woring all all methods");
     return 0;
 }
